1f8d10bbe80e649a11eba9fad7ceb61f62330544
RusselJay27 <ponferrada.russel27@gmail.com>
2020-07-23 08:55:36 +0800

    Add SignUp Class library for Registration and Reset Class Library for Change Password.

---
+++ Users/Reset.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using DataVice_PCL.Users.Struct;
+using System.Linq;
+
+namespace DataVice_PCL.Users
+{
+    class Reset
+    {
+        #region Fields
+        /// <summary>
+        /// Instance for Change Password Class.
+        /// </summary>
+        private Reset instance;
+        public Reset Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Reset();
+                    return instance;
+            }
+        }
+
+        /// <summary>
+        /// Web service for comunication for our Backend.
+        /// </summary>
+
+        HttpClient client;
+
+        #endregion
+
+        #region Consructor
+        public Reset()
+        {
+            client = new HttpClient();
+        }
+        public async void Submit(string activation_key, string password, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+                dict.Add("AK", activation_key);
+                dict.Add("PW", password);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/datavice/api/v1/user/reset", content);
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ Users/SignUp.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using System.Net.Http;
+using DataVice_PCL.Users.Struct;
+
+namespace DataVice_PCL.Users
+{
+
+    public class SignUp
+    {
+        #region Fields
+
+        /// <summary>
+        /// Instance of SignUp Class.
+        /// </summary>
+
+        private static SignUp instance;
+        public static SignUp Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new SignUp();
+                return instance;
+            }
+        }
+        
+
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+
+        #endregion
+
+        #region Constructor
+        public SignUp()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+
+        public async void Submit(string username, string email, string fullname, string lastname, string gender, string province, string city,  Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+                dict.Add("UN", username);
+                dict.Add("EM", email);
+                dict.Add("FN", fullname);
+                dict.Add("LN", lastname);
+                dict.Add("GD", gender);
+                dict.Add("PR", province);
+                dict.Add("CT", city);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/datavice/api/v1/user/signup", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+    }
+}
