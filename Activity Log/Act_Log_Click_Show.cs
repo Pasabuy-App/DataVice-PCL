bfeabc58bea36d03f40d6d3a7f3c4a8a15df6d6f
RusselJay27 <ponferrada.russel27@gmail.com>
2020-07-24 15:46:27 +0800

    Added Activity Log Fetch, Fetch_Feed, Click_Show Class.

---
+++ Activity Log/Act_Log_Click_Show.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using DataVice_PCL.Users.Struct;
+using System.Net.Http;
+
+namespace DataVice_PCL.Activity_Log
+{
+    public class Activity_Log_ByID
+    {
+        #region Fields
+        /// <summary>
+        /// Instance for Act_Log_Create Class.
+        /// </summary>
+        private static Activity_Log_ByID instance;
+        public static Activity_Log_ByID Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Activity_Log_ByID();
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communcation for our Backend.
+        /// </summary>
+        HttpClient client;
+        public Activity_Log_ByID()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Method
+        public async void Create(string wpid, string session_key, string activity_id, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wpid);
+            dict.Add("snky", session_key);
+            dict.Add("atid", activity_id);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/datavice/api/v1/activity/get_activity_byid", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}
---
+++ Activity Log/Act_Log_Fetch.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using DataVice_PCL.Users.Struct;
+using System.Net.Http;
+
+namespace DataVice_PCL.Activity_Log
+{
+    public class Activity_Log_Fetch
+    {
+        #region Fields
+        /// <summary>
+        /// Instance for Activity_Log_Fetch Class.
+        /// </summary>
+        private static Activity_Log_Fetch instance;
+        public static Activity_Log_Fetch Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Activity_Log_Fetch();
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communcation for our Backend.
+        /// </summary>
+        HttpClient client;
+        public Activity_Log_Fetch()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Method
+        public async void GetData(string wpid, string session_key, Action<bool, string> callback)
+        {
+            string getRequest = "?";
+            getRequest += "wpid" + wpid;
+            getRequest += "&snky" + session_key;
+
+            var response = await client.GetAsync(BaseClass.BaseDomainUrl + "/datavice/api/v1/activity/get_activity");
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}
